b1680fedd46a75d78466cf1ec066d3be44c4eab0
Mateus <mateuskvasconcelos@gmail.com>
2023-01-10 21:27:06 -0300

    Feature: Validate with Joi works in the method POST /participants

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import express from "express";
 import cors from "cors";
+import Joi from "joi";
+
+// const Joi = require("joi");
+
+const schema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+});
 
 const app = express();
 const PORT = 5001;
@@ -7,8 +14,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  return res.status(200).send("OK");
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  try {
+    await schema.validateAsync({
+      name,
+    });
+    return res.status(200).send("OK");
+  } catch (err) {
+    return res.sendStatus(422);
+  }
 });
 
 app.listen(PORT, () => {
